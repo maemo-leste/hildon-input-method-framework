e5ae1edd0e420acaf0b163df40a15da063ab4f5e
Arthur Demchenkov <spinal.by@gmail.com>
2020-01-31 12:01:38 +0300

    Revert "Don't ignore Enter key presses"
    The proper fix is here:
    https://github.com/maemo-leste/hildon-input-method/commit/f01d4721c9bc65202cb2f601f815f7e8c797d8c2

--- src/hildon-im-context.c
+++ src/hildon-im-context.c
@@ -2076,24 +2076,29 @@ process_enter_key (HildonIMContext *context, GdkEventKey *event)
                                      event->keyval, event->hardware_keycode);
 
   /* Enter advances focus as if tab was pressed */
-  if (g_type_class_peek(GTK_TYPE_ENTRY) != NULL)
+  if (event->keyval == GDK_KP_Enter || event->keyval == GDK_ISO_Enter)
   {
-    if (g_signal_handler_find(context->client_gtk_widget,
-                              G_SIGNAL_MATCH_ID,
-                              g_signal_lookup("activate", GTK_TYPE_ENTRY),
-                              0, NULL, NULL, NULL))
-      return FALSE;
+    if (g_type_class_peek(GTK_TYPE_ENTRY) != NULL)
+    {
+      if (g_signal_handler_find(context->client_gtk_widget,
+                                G_SIGNAL_MATCH_ID,
+                                g_signal_lookup("activate", GTK_TYPE_ENTRY),
+                                0, NULL, NULL, NULL))
+        return FALSE;
+    }
+
+    if (GTK_IS_ENTRY(context->client_gtk_widget) &&
+        !gtk_entry_get_activates_default(GTK_ENTRY(context->client_gtk_widget)))
+    {
+      hildon_im_gtk_focus_next_text_widget(context->client_gtk_widget,
+                                           GTK_DIR_TAB_FORWARD);
+      return TRUE;
+    }
+
+    return FALSE;
   }
 
-  if (GTK_IS_ENTRY(context->client_gtk_widget) &&
-      !gtk_entry_get_activates_default(GTK_ENTRY(context->client_gtk_widget)))
-  {
-    hildon_im_gtk_focus_next_text_widget(context->client_gtk_widget,
-                                         GTK_DIR_TAB_FORWARD);
-    return TRUE;
-  }
-
-  return FALSE;
+  return TRUE;
 }
 
 static gboolean
